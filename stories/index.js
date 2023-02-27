cae532b80cc87454347cc65eb7ae71e050e79afb
Braden <braden.clokie@gmail.com>
2023-02-26 21:16:33 -0500

    Added form to Appointments

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,8 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -172,5 +174,21 @@ storiesOf('Appointment', module)
     <Error
       message={'Could not delete appointment.'}
       onClose={action('onClose')}
+    />
+  ))
+  .add('Form Edit', () => (
+    <Form
+      student={'Cody Hilborn'}
+      interviewer={3}
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+  ))
+  .add('Form Create', () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
     />
   ));
