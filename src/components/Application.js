478f88e2d2585ce23fb666ec56428dc966b833d7
Braden <braden.clokie@gmail.com>
2023-02-26 23:17:40 -0500

    Implemented axios to retrieve days

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,24 +5,6 @@ import "components/Application.scss";
 import 'components/Appointment';
 import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 const appointments = {
   "1": {
     id: 1,
@@ -64,6 +46,11 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/days')
+      .then(response => setDays(response.data));
+  }, []);
   const appointmentArr = Object.values(appointments).map((appointment) => {
     return <Appointment
       key={appointment.id}
