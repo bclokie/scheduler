2ae132624da2c018f318ea6744106d4834b5ee67
Braden <braden.clokie@gmail.com>
2023-03-02 12:35:46 -0500

    Bug fixes and refactor

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,6 +5,10 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
+    cy.contains("[data-testid=day]", "Tuesday")
+    .click()
+    .should("have.css", "background-color", "rgb(242, 242, 242)");
+
   });
   
 });
--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,25 +1,18 @@
 import React from "react";
-import DayListItem from './DayListItem'
-
+import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  const {days} = props
-
-  const parsedDays = 
-    Array.isArray(days) &&
-    days.map((day) => (
+  const parsedDays =
+    Array.isArray(props.days) &&
+    props.days.map((day) => (
       <DayListItem
         key={day.id}
-        name={day.name} 
-        spots={day.spots} 
-        selected={day.name === props.value}
-        setDay={props.setDay} 
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
       />
-    ))
+    ));
 
-  return (
-    <ul>
-      {parsedDays}
-    </ul>
-  );
+  return <ul>{parsedDays}</ul>;
 }
